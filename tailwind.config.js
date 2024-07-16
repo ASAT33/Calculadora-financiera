d852e9609b1c77d891937ef6f44a0511f7d1436b
Pablo Delgado <pablo.delgado@utp.ac.pa>
2024-07-16 01:39:12 -0500

    Configuracion con Tailwind los estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './public/**/*.html',  // Busca todos los archivos HTML en la carpeta public
+    './public/js/**/*.js', // Busca todos los archivos JS en la carpeta public/js
   ],
   theme: {
     extend: {},
